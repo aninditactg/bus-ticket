98cdbe98a4ece051feea9a0090913d34a993ddfa
Eva Kalam <israteva525@gmail.com>
2025-09-02 00:30:34 +0600

    Table modify :Eva

--- database/migrations/2025_09_01_180134_create_routes_table.php
+++ database/migrations/2025_09_01_180134_create_routes_table.php
@@ -10,21 +10,18 @@ return new class extends Migration
      * Run the migrations.
      */
     public function up(): void
-{
-    Schema::create('feedbacks', function (Blueprint $table) {
-        $table->id();
-        $table->string('name')->nullable();
-        $table->string('email')->nullable();
-        $table->text('message');
-        $table->timestamps();
-    });
-}
+    {
+        Schema::create('routes', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
 
     /**
      * Reverse the migrations.
      */
     public function down(): void
     {
-        Schema::dropIfExists('feedback');
+        Schema::dropIfExists('routes');
     }
 };
